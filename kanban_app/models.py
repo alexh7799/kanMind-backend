104dbdc812b58cdb1a7f205d86f7ac0138a8bcc9
Alexander Hörst <hoerst.alexander@gmail.com>
2025-05-03 20:03:29 +0200

    add user_auth_app and edit kanban_app.models

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Boards(models.Model):
     title = models.CharField(max_length=100)
     members = models.ManyToManyField('auth.User', related_name='board_members')
@@ -16,9 +15,9 @@ class Tasks(models.Model):
     board = models.ForeignKey(Boards, related_name='tasks', on_delete=models.CASCADE)
     status = models.CharField(max_length=20, choices=[('to-do', 'To-Do'), ('in-progress', 'In-Progress'), ('review', 'Review'), ('done', 'Done')], default='to-do')
     priority = models.CharField(max_length=20, choices=[('low', 'Low'), ('medium', 'Medium'), ('high', 'High')], default='medium')
-    assignee = models.ForeignKey('auth.User', related_name='assigned_tasks', on_delete=models.CASCADE)
-    reviewer = models.ForeignKey('auth.User', related_name='reviewed_tasks', on_delete=models.CASCADE, null=True, blank=True)
-    due_date = models.DateTimeField(null=True, blank=True)
+    assignee_id = models.ForeignKey('auth.User', related_name='assigned_tasks', on_delete=models.CASCADE, null=True, blank=True)
+    reviewer_id = models.ForeignKey('auth.User', related_name='reviewed_tasks', on_delete=models.CASCADE)
+    due_date = models.CharField(max_length=80, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -30,6 +29,10 @@ class TaskComments(models.Model):
     user = models.ForeignKey('auth.User', related_name='comments', on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def author(self):
+        return f"{self.user.first_name} {self.user.last_name}"
 
     def __str__(self):
         return f"Comment by {self.user.username} on {self.task.title}"
